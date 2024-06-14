075e9e346bbc0dbd220dc93a1bea2e69a1ae0a72
KevinsitoHB <family-hernandez2014@outlook.com>
2024-06-14 19:56:03 -0400

    token

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { HomeComponent } from './components/home/home.component';
 import { JobsComponent } from './components/jobs/jobs.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { activateGuard } from './guards/activate.guard';
 
 export const routes: Routes = [
   { path: 'home', title: 'Home', component: HomeComponent },
   { path: 'login', title: 'Login', component: LoginComponent },
-  { path: 'jobs', title: 'Jobs', component: JobsComponent },
+  {
+    path: 'jobs',
+    title: 'Jobs',
+    component: JobsComponent,
+    canActivate: [activateGuard],
+  },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', title: '404 Page not Found', component: PageNotFoundComponent },
 ];
--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -18,4 +18,15 @@ export class LoginService {
   validateToken(token: String) {
     return this.httpClient.get(`${this.API_URL}/${token}`);
   }
+
+  isLoggued() {
+    if (localStorage.getItem('token')) {
+      console.log('Is Loggued in :>> ');
+      return true;
+    } else {
+      console.log('Is not Loggued in :>> ');
+
+      return false;
+    }
+  }
 }

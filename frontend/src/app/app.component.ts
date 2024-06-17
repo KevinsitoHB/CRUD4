d78c1b14ba9e1b6d54ee7ae868ba40b426b43ec0
KevinsitoHB <family-hernandez2014@outlook.com>
2024-06-17 11:27:23 -0400

    avances

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,14 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { CommonModule } from '@angular/common';
+import { toast, NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationComponent],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, NgxSonnerToaster],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  template: `
+    <ngx-sonner-toaster />
+    <button (click)="toast('My first toast')">Give me a toast</button>
+  `,
 })
 export class AppComponent {
+  protected readonly toast = toast;
   title = 'frontend';
 }

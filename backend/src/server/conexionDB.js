549f3610a12ef83f576c53058f6b446e0dd4b060
KevinsitoHB <family-hernandez2014@outlook.com>
2024-06-22 12:57:40 -0400

    xl

--- backend/src/server/conexionDB.js
+++ backend/src/server/conexionDB.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 mongoose
-  .connect(process.env.MONDGODB_ATLAS_URI)
-  .then(() => {
-    console.log('Conectado a Mongoose');
-  })
-  .catch((error) => {
-    console.log('Fallo en la conexion: ' + error);
-  });
+	.connect(process.env.MONGODB_ATLAS_URI)
+	.then(() => {
+		console.log('Conectado a Mongoose');
+	})
+	.catch((error) => {
+		console.log('Fallo en la conexion: ' + error);
+	});
